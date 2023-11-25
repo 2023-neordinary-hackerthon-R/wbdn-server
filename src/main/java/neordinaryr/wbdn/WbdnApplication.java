98507a7a50cf36244cefcba3913d3970ff117ef2
DDeo99 <yoondukwoo@gmail.com>
2023-11-25 17:13:01 +0900

    @EnableJpaAuditing

--- src/main/java/neordinaryr/wbdn/WbdnApplication.java
+++ src/main/java/neordinaryr/wbdn/WbdnApplication.java
@@ -2,14 +2,15 @@ package neordinaryr.wbdn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WbdnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WbdnApplication.class, args);
 	}
-
 
 
 }
