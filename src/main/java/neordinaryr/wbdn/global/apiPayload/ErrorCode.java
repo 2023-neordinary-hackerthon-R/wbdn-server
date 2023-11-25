b1d38d471e2d9fb1ee5becd4226f47652df3571e
enble <enble_777@naver.com>
2023-11-25 22:06:42 +0900

    feat: NO_SUCH_COMMENT_ERROR 에러코드 추가
    제공된 댓글 ID에 해당하는 댓글이 없는 경우의 에러코드 추가

--- src/main/java/neordinaryr/wbdn/global/apiPayload/ErrorCode.java
+++ src/main/java/neordinaryr/wbdn/global/apiPayload/ErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum ErrorCode implements BaseCode {
-    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "GLOBAL_001", "");
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "GLOBAL_001", ""),
+
+    // Comment 관련
+    NO_SUCH_COMMENT_ERROR(HttpStatus.BAD_REQUEST, "COMMENT_001", "해당하는 ID를 가진 댓글이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
