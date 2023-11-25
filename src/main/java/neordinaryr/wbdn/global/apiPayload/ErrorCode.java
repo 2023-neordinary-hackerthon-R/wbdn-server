d412c94539e27f6adcb8490012ef9d9ebd2b0fca
eomgerm <fishbread00@gmail.com>
2023-11-25 22:52:08 +0900

    :sparkles: Feat: S3 관련 에러 추가

--- src/main/java/neordinaryr/wbdn/global/apiPayload/ErrorCode.java
+++ src/main/java/neordinaryr/wbdn/global/apiPayload/ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum ErrorCode implements BaseCode {
-    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "GLOBAL_001", "");
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "GLOBAL_001", ""),
+    S3_OBJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "S3_001", "S3에서 해당 파일을 찾지 못했습니다."),
+    FILE_NOT_EXISTS(HttpStatus.BAD_REQUEST, "POST_001", "게시글 사진이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
